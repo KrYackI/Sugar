2c737249f346ed36e1d4276b27fcc94cfca5bbf2
Alex <x.kryack.x@gmail.com>
2024-04-07 19:18:16 +0300

    some OK(?) interface

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -3,19 +3,19 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QApplication, QTableWidget, QP
 from PyQt5.QtGui import QLinearGradient, QColor
 import functional_interface as fi
 
-
 def dialog_window(text: str):
     dlg = QMessageBox()
     dlg.setInformativeText(text)
     dlg.setStyleSheet("QLabel{ color: gray}")
     dlg.exec()
 
+
 class LabaInterface(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Full Screen Window")
-        self.resize(1480, 1080)
+        self.resize(1480, 960)
         # self.showFullScreen()
 
         self.setStyleSheet("background-color: white;")
@@ -38,11 +38,35 @@ class LabaInterface(QWidget):
         self.setLayout(main_layout)
 
         self.sublayout1 = QGridLayout()
-        self.sublayout2 = QGridLayout()
         self.sublayout3 = QGridLayout()
         main_layout.addLayout(self.sublayout1, 0)
-        main_layout.addLayout(self.sublayout2, 1)
-        main_layout.addLayout(self.sublayout3, 2)
+        main_layout.addLayout(self.sublayout3, 1)
+
+        self.result1 = QLineEdit()
+        self.result2 = QLineEdit()
+        self.result3 = QLineEdit()
+        self.result4 = QLineEdit()
+        self.result5 = QLineEdit()
+        self.result6 = QLineEdit()
+        self.result1.setReadOnly(True)
+        self.result2.setReadOnly(True)
+        self.result3.setReadOnly(True)
+        self.result4.setReadOnly(True)
+        self.result5.setReadOnly(True)
+        self.result6.setReadOnly(True)
+        self.result1.hide()
+        self.result2.hide()
+        self.result3.hide()
+        self.result4.hide()
+        self.result5.hide()
+        self.result6.hide()
+
+        self.sublayout3.addWidget(self.result1, 7, 0, 1, 3)
+        self.sublayout3.addWidget(self.result2, 8, 0, 1, 3)
+        self.sublayout3.addWidget(self.result3, 9, 0, 1, 3)
+        self.sublayout3.addWidget(self.result4, 10, 0, 1, 3)
+        self.sublayout3.addWidget(self.result5, 11, 0, 1, 3)
+        self.sublayout3.addWidget(self.result6, 12, 0, 1, 3)
 
 
         self.manual_matrix_input_button = QPushButton("Создать матрицу вручную")
@@ -62,7 +86,24 @@ class LabaInterface(QWidget):
         self.create_manual_count = 0
         self.create_auto_count = 0
 
+        self.graphicsView = PlotWidget()
+        self.sublayout3.addWidget(self.graphicsView, 0, 1)
+        self.graphicsView.hide()
+
     def choose_mode(self):
+        self.result1.hide()
+        self.result2.hide()
+        self.result3.hide()
+        self.result4.hide()
+        self.result5.hide()
+        self.result6.hide()
+        self.result1.clear()
+        self.result2.clear()
+        self.result3.clear()
+        self.result4.clear()
+        self.result5.clear()
+        self.result6.clear()
+
         if self.create_manual_count != 0:
             self.manual_matrix_input_button.show()
             self.auto_matrix_input_button.show()
@@ -70,8 +111,6 @@ class LabaInterface(QWidget):
 
             self.matrix_size.hide()
             self.matrix_size.clear()
-            # self.matrix_col_size.hide()
-            # self.matrix_col_size.clear()
             self.create_matrix_button.hide()
             self.matrix_name.hide()
 
@@ -81,19 +120,6 @@ class LabaInterface(QWidget):
                 self.manual_result_button.hide()
 
 
-            if self.manual_res_count != 0:
-                self.result1.hide()
-                self.result2.hide()
-                self.result3.hide()
-                self.result4.hide()
-                self.result5.hide()
-                self.result6.hide()
-                self.result1.clear()
-                self.result2.clear()
-                self.result3.clear()
-                self.result4.clear()
-                self.result5.clear()
-                self.result6.clear()
 
             self.create_manual_count = 0
             self.manual_res_count = 0
@@ -108,11 +134,11 @@ class LabaInterface(QWidget):
             self.choose_mode_button.hide()
 
             self.first_string_name.hide()
-            # self.auto_matrix_col_size.hide()
+
             self.second_string_name.hide()
-            # self.auto_matrix_col_size.hide()
+
             self.auto_matrix_size.hide()
-            # self.auto_matrix_col_size.hide()
+
 
             self.min_size1.hide()
             self.max_size1.hide()
@@ -121,9 +147,6 @@ class LabaInterface(QWidget):
             self.min_size3.hide()
             self.max_size3.hide()
             self.auto_result_button.hide()
-
-            if self.auto_res_count != 0:
-                self.result7.hide()
 
             self.create_manual_count = 0
             self.manual_res_count = 0
@@ -140,9 +163,10 @@ class LabaInterface(QWidget):
 
 
         self.first_string_name = QLineEdit("Введите размер матрицы")
+        self.first_string_name.setReadOnly(True)
         self.second_string_name = QLineEdit("Введите ограничения параметров")
+        self.second_string_name.setReadOnly(True)
         self.auto_matrix_size = QLineEdit()
-        # self.auto_matrix_col_size = QLineEdit()
         self.min_size1 = QLineEdit()
         self.max_size1 = QLineEdit()
         self.min_size2 = QLineEdit()
@@ -150,15 +174,11 @@ class LabaInterface(QWidget):
         self.min_size3 = QLineEdit()
         self.max_size3 = QLineEdit()
 
-        self.result7 = QLineEdit()
-        self.result7.hide()
-
         self.auto_result_button = QPushButton("Решить")
         self.auto_result_button.clicked.connect(lambda: self.result("auto"))
 
-        self.sublayout3.addWidget(self.first_string_name, 0, 0, 1, 2)
+        self.sublayout3.addWidget(self.first_string_name, 0, 0, 1, 1)
         self.sublayout3.addWidget(self.auto_matrix_size, 1, 0)
-        # self.sublayout3.addWidget(self.auto_matrix_col_size, 1, 1)
         self.sublayout3.addWidget(self.second_string_name, 2, 0, 1, 2)
         self.sublayout3.addWidget(self.min_size1, 3, 0)
         self.sublayout3.addWidget( self.max_size1, 3, 1)
@@ -167,7 +187,6 @@ class LabaInterface(QWidget):
         self.sublayout3.addWidget(self.min_size3, 5, 0)
         self.sublayout3.addWidget( self.max_size3, 5, 1)
         self.sublayout3.addWidget(self.auto_result_button, 6, 0, 1, 2)
-        self.sublayout3.addWidget(self.result7, 7, 0, 1, 2)
 
 
 
@@ -182,18 +201,16 @@ class LabaInterface(QWidget):
         self.manual_res_count = 0
 
         self.matrix_name = QLineEdit()
-        # matrix_name.setReadOnly()
         self.matrix_name.setText("Введите размеры матрицы")
+        self.matrix_name.setReadOnly(True)
         self.matrix_size = QLineEdit()
-        # self.matrix_col_size = QLineEdit()
 
         self. create_matrix_button = QPushButton("Создать")
         self.create_matrix_button.clicked.connect(lambda: self.create_matrix(self.matrix_size))
 
-        self.sublayout2.addWidget(self.matrix_name, 0, 0, 1, 3)
-        self.sublayout2.addWidget(self.matrix_size, 1, 0)
-        # self.sublayout2.addWidget(self.matrix_col_size, 1, 1)
-        self.sublayout2.addWidget(self.create_matrix_button, 1, 2)
+        self.sublayout3.addWidget(self.matrix_name, 0, 0, 1, 3)
+        self.sublayout3.addWidget(self.matrix_size, 1, 0)
+        self.sublayout3.addWidget(self.create_matrix_button, 1, 2)
 
     def create_matrix(self, size):
         try:
@@ -209,39 +226,25 @@ class LabaInterface(QWidget):
         if self.matrix_count == 0:
             self.matrix = QTableWidget()
             self.matrix_count += 1
+            self.matrix.horizontalHeader().setVisible(False)
+            self.matrix.verticalHeader().setVisible(False)
 
         self.matrix.show()
-        self.matrix.horizontalHeader().setVisible(False)
-        self.matrix.verticalHeader().setVisible(False)
 
         self.matrix.setRowCount(int(size.text()))
         self.matrix.setColumnCount(int(size.text()))
+        self.matrix.resizeRowsToContents()
 
-        self.sublayout2.addWidget(self.matrix, 2, 0, 1, 3)
-        self.matrix.setMaximumSize(int(size.text()) * 130, int(size.text()) * 46)
 
-        self.result1 = QLineEdit()
-        self.result2 = QLineEdit()
-        self.result3 = QLineEdit()
-        self.result4 = QLineEdit()
-        self.result5 = QLineEdit()
-        self.result6 = QLineEdit()
-        self.result1.hide()
-        self.result2.hide()
-        self.result3.hide()
-        self.result4.hide()
-        self.result5.hide()
-        self.result6.hide()
-        self.manual_result_button = QPushButton("Решить")
-        self.manual_result_button.clicked.connect(lambda: self.result("manual"))
+        self.matrix.setMaximumSize(self.matrix.columnCount() * self.matrix.columnWidth(int(size.text()) - 1) + 2, self.matrix.rowCount() * self.matrix.rowHeight(int(size.text()) - 1) + 4)
+        self.sublayout3.addWidget(self.matrix, 2, 0, 1, 3)
 
-        self.sublayout2.addWidget(self.manual_result_button, 3, 0, 1, 3)
-        self.sublayout2.addWidget(self.result1, 4, 0, 1, 3)
-        self.sublayout2.addWidget(self.result2, 5, 0, 1, 3)
-        self.sublayout2.addWidget(self.result3, 6, 0, 1, 3)
-        self.sublayout2.addWidget(self.result4, 7, 0, 1, 3)
-        self.sublayout2.addWidget(self.result5, 8, 0, 1, 3)
-        self.sublayout2.addWidget(self.result6, 9, 0, 1, 3)
+        if not hasattr(self, 'manual_result_button'):
+            self.manual_result_button = QPushButton("Решить")
+            self.manual_result_button.clicked.connect(lambda: self.result("manual"))
+
+            self.sublayout3.addWidget(self.manual_result_button, 3, 0, 1, 3)
+        self.manual_result_button.show()
 
     def result(self, type):
         if type == "manual":
@@ -283,16 +286,16 @@ class LabaInterface(QWidget):
 
             res = fi.manual(int(self.matrix_size.text()), m)
 
-            self.result1.setText("Результат 1 = " + str(res[0]))
-            self.result2.setText("Результат 2 = " + str(res[1]))
-            self.result3.setText("Результат 3 = " + str(res[2]))
-            self.result4.setText("Результат 4 = " + str(res[3]))
-            self.result5.setText("Результат 5 = " + str(res[4]))
-            self.result6.setText("Результат 6 = " + str(res[5]))
+            self.result1.setText(res[0])
+            self.result2.setText(res[1])
+            self.result3.setText(res[2])
+            self.result4.setText(res[3])
+            self.result5.setText(res[4])
+            self.result6.setText(res[5])
         elif (type == "auto"):
             self.auto_res_count = 1
             try:
-                self.int_valid([self.auto_matrix_size.text()], 1)
+                self.int_valid([self.auto_matrix_size.text()], 0) # поменять на 1
                 self.float_valid([self.min_size1.text(),
                                   self.max_size1.text(),
                                   self.min_size2.text(),
@@ -307,16 +310,31 @@ class LabaInterface(QWidget):
                 info_msg.setIcon(QMessageBox.Icon.Critical)
                 info_msg.exec()
                 return
-            self.result7.show()
 
-            self.result7.setText("Результат 7")
 
-            fi.experiment(int(self.auto_matrix_size.text()), float(self.min_size1.text()),
+
+            re = fi.experiment(int(self.auto_matrix_size.text()), float(self.min_size1.text()),
                           float(self.max_size1.text()),
                           float(self.min_size2.text()) + 1.0,
                           float(self.max_size2.text()) + 1.0,
                           float(self.min_size3.text()),
                           float(self.max_size3.text()))
+
+            self.result1.setText(re[0])
+            self.result2.setText(re[1])
+            self.result3.setText(re[2])
+            self.result4.setText(re[3])
+            self.result5.setText(re[4])
+            self.result6.setText(re[5])
+
+            self.result1.show()
+            self.result2.show()
+            self.result3.show()
+            self.result4.show()
+            self.result5.show()
+            self.result6.show()
+
+
 
     def float_valid(self, floatList, flag):
         for param in floatList:
@@ -335,19 +353,31 @@ class LabaInterface(QWidget):
         return True
 
     def int_valid(self, intList, flag):
-        for num in intList:
+        try:
+            int(intList[0])
+        except ValueError:
+            raise Exception("Размер матрицы должен быть целочисленным")
+        if (int(intList[0]) <= 0):
+            raise Exception("Размер матрицы должен быть больше нуля")
+        if (flag):
             try:
-                int(num)
+                int(intList[1])
             except ValueError:
-                raise Exception("Размер матрицы должет быть целочисленным")
-            if (int(num) < 0):
-                raise Exception("Размер матрицы должет быть больше нуля")
+                raise Exception("Число экспериментов должно быть целочисленным")
+            if (int(intList[1]) <= 0):
+                raise Exception("Число экспериментов должно быть больше нуля")
         return True
 
     def paintEvent(self, event):
         self.setStyleSheet("border: 1px solid;"
                            "border-color : black;"
                            "font-size: 26px;")
+
+
+    # def show_graph(self, summa):
+    #     x = [i + 1 for i in range(len(summa[0]))]
+    #     chart = Canvas(self, x, summa)
+
 
 if __name__ == '__main__':
     try:
